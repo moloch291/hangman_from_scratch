4ca53c0e133fb90b34131a44c4e06d09adde754f
root <danielkomjati1@gmail.com>
2021-02-03 11:54:11 +0100

    Updated to advanced version!

---
+++ life_status.py
@@ -0,0 +1,173 @@
+def logo():
+    logo = """888                                                           
+888                                                           
+888                                                             
+88888b.  8888b. 88888b.  .d88b.  88888b.d88b.  8888b.  88888b.  
+888 "88b    "88b888 "88bd88P"88b 888 "888 "88b    "88b 888 "88b 
+888  888.d888888888  888888  888 888  888  888.d888888 888  888 
+888  888888  888888  888Y88b 888 888  888  888888  888 888  888 
+888  888"Y888888888  888 "Y88888 888  888  888"Y888888 888  888 
+                             888                                
+                        Y8b d88P                                
+                         "Y88P"                                 """
+    return logo
+
+
+def lives_status_1():
+    life_status_1 = '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+========='''
+    return life_status_1
+
+
+def lives_status_2():
+    life_status_2 = '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+========='''
+    return life_status_2
+
+
+def lives_status_3():
+    life_status_3 = '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+========='''
+    return life_status_3
+
+
+def lives_status_4():
+    life_status_4 = '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+========='''
+    return life_status_4
+
+
+def lives_status_5():
+    life_status_5 = '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+========='''
+    return life_status_5
+
+
+def lives_status_6():
+    life_status_6 = '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+========='''
+    return life_status_6
+
+
+def critical_moment():
+    print("   (/)")
+    print("   (/)")
+    print("     (/)")
+    print("   (/)")
+    print("    (/)")
+    print("   (/)")
+    print("   (/))")
+    print("  (/)(/)")
+    print(" (/)'`(/)")
+    print("(/)    (/)\n(/)    (/)")
+    print("(/)    (/)")
+    print("(/)    (/)")
+    print(" (/)  (/)")
+    print("  (/)(/)")
+    print("   `""`")
+    print("")
+    print("  ,%%%%%%%%,")
+    print(" %%o%%/%%%%%%")
+    print("%%%\%%%<%%%%%")
+    print("%%>%%%/%%%%o%%")
+    print("%%%%%o%%\%%//%")
+    print("%\o%\%%/%o/%%'")
+    print(" '%%\ `%/%%%'")
+    print("   '%| |%|%'")
+    print("     | | (O")
+    print("     | | |\ ")
+    print("     | | >>")
+    print("     | |")
+    print("    /   \ ")
+    print(" ^^^^^^^^^^^^^^^^^")
+
+
+def dead_status():
+    print("   |_______________``\ ")
+    print("    [/]           [  ]")
+    print("    [\]           | ||")
+    print("    [/]           |  |")
+    print("    [\]           |  |")
+    print("    [/]           || |")
+    print("   [---]          |  |")
+    print("   [---]          |@ |")
+    print("   [---]          |  |")
+    print("  oOOOOOo         |  |")
+    print(" oOO___OOo        | @|")
+    print("oO/|||||\Oo       |  |")
+    print("OO/|||||\OOo      |  |")
+    print("*O\ x x /OO*      |  |")
+    print("/*|  c  |O*\      |  |")
+    print("   \_O_/    \     |  |")
+    print("    \#/     |     |  |")
+    print(" |       |  |     | ||")
+    print(" |       |  |_____| ||__")
+    print("_/_______\__|  \  ||||  \ ")
+    print("/         \_|\  _ | ||\  \ ")
+    print("|    V    |\  \//\  \  \  \ ")
+    print("|    |    | __//  \  \  \  \ ")
+    print("|    |    |\|//|\  \  \  \  \ ")
+    print("------------\--- \  \  \  \  \ ")
+    print("\  \  \  \  \  \  \  \  \  \  \ ")
+    print("_\__\__\__\__\__\__\__\__\__\__\ ")
+    print("__|__|__|__|__|__|__|__|__|__|__| ")
+    print("|___| |___| ")
+    print("|###/ \###| ")
+    print("\##/   \##/ ")
+    print(" ``     `` ")
+
+
+def get_win_ascii():
+    win_ascii = """db    db  .d88b.  db    db      db       .d88b.  .d8888. d888888b db 
+`8b  d8' .8P  Y8. 88    88      88      .8P  Y8. 88'  YP `~~88~~' 88 
+ `8bd8'  88    88 88    88      88      88    88 `8bo.      88    YP 
+   88    88    88 88    88      88      88    88   `Y8b.    88       
+   88    `8b  d8' 88b  d88      88booo. `8b  d8' db   8D    88    db 
+   YP     `Y88P'  ~Y8888P'      Y88888P  `Y88P'  `8888Y'    YP    YP """
+    return win_ascii
+
+
+def get_loose_ascii():
+    loose_ascii = """db    db  .d88b.  db    db      db   d8b   db  .d88b.  d8b   db db 
+`8b  d8' .8P  Y8. 88    88      88   I8I   88 .8P  Y8. 888o  88 88 
+ `8bd8'  88    88 88    88      88   I8I   88 88    88 88V8o 88 YP 
+   88    88    88 88    88      Y8   I8I   88 88    88 88 V8o88    
+   88    `8b  d8' 88b  d88      `8b d8'8b d8' `8b  d8' 88  V888 db 
+   YP     `Y88P'  ~Y8888P'       `8b8' `8d8'   `Y88P'  VP   V8P YP """
+    return loose_ascii
